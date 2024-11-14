235e6f6570c85b3165412cadadf86725b63b8e3a
Brian Aydemir <baydemir@morgridge.org>
2024-11-14 17:18:46 +0000

    WIP: Convert and visualize many NetCDF files

--- examples/cmip6-convert-many-with-viz/convert_dataset.py
+++ examples/cmip6-convert-many-with-viz/convert_dataset.py
@@ -1,33 +1,40 @@
-import datetime
 import os
+import pathlib
 import shutil
 import sys
 
 import numpy as np
 import OpenVisus as ov
-import openvisuspy
 import xarray as xr
 
-destination_dir = sys.argv[1]
-ds_object = os.path.basename(sys.argv[2])
-pelican_loc = sys.argv[3]
+ds_object = sys.argv[1]
+output_dir = sys.argv[2]
 
 
-## Parse metadata.
+## HACK: Use our token instead of the one from the AP.
 
-ds_parameters = os.path.splitext(ds_object)[0].split("_")
-version = ds_parameters.pop(-1) if ds_parameters[-1][0] == "v" else None
-year = int(ds_parameters.pop(-1))
-if version:
-    ds_parameters.append(version)
-field, day, model, scenario, r, g = ds_parameters
-ds_parameters = "_".join(ds_parameters)
+cred_dir = pathlib.Path(os.environ.get("_CONDOR_CREDS", "."))
+shutil.copyfile("osdf.token", cred_dir / "scitokens.use")
+
+
+## Determine the field name.
+
+filename, extension = os.path.splitext(ds_object)
+file_parts = filename.split("_")
+
+if file_parts[-1].startswith("v"):
+    year = int(file_parts.pop(-2))
+else:
+    year = int(file_parts.pop(-1))
+
+field = file_parts[0]
+field_name = "_".join(file_parts)
 
 
 ## Define the conversion's parameters.
 
-visus_idx_file = f"{destination_dir}/{ds_object}.idx"
-visus_data_dir = f"{destination_dir}/openvisus"
+visus_idx_file = f"{output_dir}/{ds_object}.idx"
+visus_data_dir = f"{output_dir}/{ds_object}"
 arco = "2mb"
 
 
@@ -43,11 +50,12 @@ data = ds[field][...].values
 vmin, vmax = np.min(data), np.max(data)
 
 ov_field = ov.Field.fromString(
-    f"{ds_parameters} {str(data.dtype)} format(row_major) min({vmin}) max({vmax})"
+    f"{field_name} {str(data.dtype)} format(row_major) min({vmin}) max({vmax})"
 )
 
+print(f"data.shape: {data.shape}")
 D, H, W = data.shape
-assert D == 365  # ensure that the NetCDF file covers 365 days
+D = min(D, 364)  # look at only 365 days (0..364)
 
 db = ov.CreateIdx(
     url=visus_idx_file,
@@ -64,24 +72,6 @@ db = ov.CreateIdx(
 
 for Z in range(D):  # each Z is a day (offset from the year)
     slice = data[Z, ...]
-    db.write(slice, time=year * 365 + Z, field=ds_parameters)
+    db.write(slice, time=year * 365 + Z, field=field_name)
 
 db.compressDataset(["zip"])
-
-
-## Copy the data into OSDF.
-
-sys.stdout.flush()
-sys.stderr.flush()
-os.execl(
-    "./pelican",
-    "pelican",
-    "object",
-    "put",
-    "--debug",
-    "--recursive",
-    "--token",
-    "osdf.token",
-    destination_dir,
-    pelican_loc,
-)
